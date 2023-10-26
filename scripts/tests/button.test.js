f70a22f0ca1922730a26bd4bcd95cd02d5ee392e
rowanam <rowanamaas@gmail.com>
2023-10-26 13:03:28 +0000

    Load entire index.html DOM into mock

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,3 +1,6 @@
+// Note: to run only tests in this file, type 'npm test button.test.js' into the CL
+// To run all tests, type 'npm test'
+
 /**
  * @jest-environment jsdom
  */
@@ -6,12 +9,20 @@ const buttonClick = require("../button");
 
 // Learning note: runs before each test
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
-})
+    // Learning note: fs - file system built into Node
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
